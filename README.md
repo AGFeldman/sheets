d37bf30c2143e956fff9781cc7c059669bd65ca8
Aaron Feldman <agf@fb.com>
2019-07-20 16:35:11 -0700

    update readme and help message

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-See also https://github.com/xflr6/gsheets and https://github.com/nithinmurali/pygsheets and https://pypi.org/project/gspread/
+```
+~> python3 sheets.py --help
+usage: sheets.py [-h] {generate_token,create_sheet,read_rows,append_row} ...
+
+Create, append to, and read from Google Sheets that are associated with a
+Google APIs project, without exposing access to any other Google Sheets or
+Google Drive data.
+
+positional arguments:
+  {generate_token,create_sheet,read_rows,append_row}
+    generate_token      Generate a token for accessing Google Drive files via
+                        a Google APIs project
+    create_sheet        Create a sheet
+    read_rows           Read rows from a sheet
+    append_row          Append a row to a sheet
+
+optional arguments:
+  -h, --help            show this help message and exit
+```
+
+For more power, consider https://github.com/xflr6/gsheets, https://github.com/nithinmurali/pygsheets, or https://pypi.org/project/gspread/
--- sheets.py
+++ sheets.py
@@ -84,7 +84,7 @@ generate_token_parser = subparsers.add_parser(
         'generate_token',
         help='Generate a token for accessing Google Drive files via a Google APIs project')
 generate_token_parser.set_defaults(func=_generate_token)
-generate_token_parser.add_argument('credentials_path', type=str, help='File path to credentials.json, which you can download after creating a project in https://console.developers.google.com/apis/. Enable the Google Sheets API in that project, then create credentials using OAuth client ID, and download the cred.')
+generate_token_parser.add_argument('credentials_path', type=str, help='File path to credentials.json, which you can download after creating a project in https://console.developers.google.com/apis/. Enable the Google Sheets API in that project, then create credentials that use OAuth client ID, and download the credentials.')
 generate_token_parser.add_argument('token_path', type=str, help='File path to use for writing token.json')
 
 create_sheet_parser = subparsers.add_parser('create_sheet', help='Create a sheet')
